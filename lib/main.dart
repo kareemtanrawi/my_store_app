253091a5561a370dafb94b6f94ea05aacf317ec3
Your Name <kareem01229526319@gmail.com>
2024-03-13 10:32:31 +0200

    finishing the setup of firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:store_app/firebase_options.dart';
 import 'package:store_app/store_app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     const StoreApp(),
   );
